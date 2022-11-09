344deef8af068858d24eab2eb6aa4086b72f51c9
grzeznx2 <grzeznx2@gmail.com>
2022-11-09 17:16:27 +0100

    Refactor

--- src/data-structures/binary-search-tree/test/BinarySearchTree.test.ts
+++ src/data-structures/binary-search-tree/test/BinarySearchTree.test.ts
@@ -1,16 +1,20 @@
 import BinarySearchTree from '../BinarySearchTree'
 
+let bst: BinarySearchTree<number>
+
+beforeEach(() => {
+  bst = new BinarySearchTree(10)
+
+  bst.add(5)
+  bst.add(15)
+  bst.add(7)
+  bst.add(12)
+  bst.add(4)
+  bst.add(18)
+})
+
 describe('BinarySearchTree', () => {
   it('properly add nodes', () => {
-    const bst = new BinarySearchTree(10)
-
-    bst.add(5)
-    bst.add(15)
-    bst.add(7)
-    bst.add(12)
-    bst.add(4)
-    bst.add(18)
-
     expect(bst.left?.value).toBe(5)
     expect(bst.right?.value).toBe(15)
     expect(bst.left?.right?.value).toBe(7)
@@ -20,15 +24,6 @@ describe('BinarySearchTree', () => {
   })
 
   it('finds node with provided value', () => {
-    const bst = new BinarySearchTree(10)
-
-    bst.add(5)
-    bst.add(15)
-    bst.add(7)
-    bst.add(12)
-    bst.add(4)
-    bst.add(18)
-
     expect(bst.find(15)?.value).toBe(15)
     expect(bst.find(12)?.value).toBe(12)
     expect(bst.find(18)?.value).toBe(18)
@@ -36,30 +31,10 @@ describe('BinarySearchTree', () => {
   })
 
   it('performs breadth first search', () => {
-    const bst = new BinarySearchTree(10)
-
-    bst.add(5)
-    bst.add(15)
-    bst.add(7)
-    bst.add(12)
-    bst.add(4)
-    bst.add(18)
-
     expect(bst.breadthFirstSearch()).toStrictEqual([10, 5, 15, 4, 7, 12, 18])
   })
 
   it('performs depth first search', () => {
-    const bst = new BinarySearchTree(10)
-
-    bst.add(5)
-    bst.add(15)
-    bst.add(7)
-    bst.add(12)
-    bst.add(4)
-    bst.add(18)
-
-    console.log(bst.depthFirstSearch())
-
     expect(bst.depthFirstSearch()).toStrictEqual([10, 5, 4, 7, 15, 12, 18])
   })
 })
