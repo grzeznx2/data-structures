401c90a6bc163afe9f631ec8eb83c04bfdc21438
grzeznx2 <grzeznx2@gmail.com>
2022-11-17 21:53:54 +0100

    Add Insertion Sort

---
+++ src/algorithms/sorting/insertion-sort/insertion-sort.ts
@@ -0,0 +1,16 @@
+function swap(i: number, j: number, array: number[]) {
+  ;[array[i], array[j]] = [array[j], array[i]]
+}
+
+function insertionSort(array: number[]) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j > 0; j--) {
+      const current = array[j]
+      const prev = array[j - 1]
+      if (prev > current) {
+        swap(j, j - 1, array)
+      } else break
+    }
+  }
+  return array
+}
