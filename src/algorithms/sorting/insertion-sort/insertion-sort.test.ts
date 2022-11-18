0941a67791dd12105335e2e0765f665b4590f7d8
grzeznx2 <grzeznx2@gmail.com>
2022-11-18 19:00:08 +0100

    Add Tests for Insertion Sort

---
+++ src/algorithms/sorting/insertion-sort/insertion-sort.test.ts
@@ -0,0 +1,21 @@
+import insertionSort from './insertion-sort'
+
+describe('Insertion Sort:', () => {
+  it('[1,2,3,4] returns [1,2,3,4]', () => {
+    expect(insertionSort([1, 2, 3, 4])).toStrictEqual([1, 2, 3, 4])
+  })
+
+  it('[1,2,3,4,2,3] returns [1,2,2,3,3,4]', () => {
+    expect(insertionSort([1, 2, 3, 4, 2, 3])).toStrictEqual([1, 2, 2, 3, 3, 4])
+  })
+
+  it('[0,-1,-2,-3,-4] returns [-4,-3,-2,-1,0]', () => {
+    expect(insertionSort([0, -1, -2, -3, -4])).toStrictEqual([-4, -3, -2, -1, 0])
+  })
+
+  it('[1,10,2,9,3,8,4,7,5,6,0] returns [0,1,2,3,4,5,6,7,8,9,10]', () => {
+    expect(insertionSort([1, 10, 2, 9, 3, 8, 4, 7, 5, 6, 0])).toStrictEqual([
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    ])
+  })
+})
--- src/algorithms/sorting/insertion-sort/insertion-sort.ts
+++ src/algorithms/sorting/insertion-sort/insertion-sort.ts
@@ -2,7 +2,7 @@ function swap(i: number, j: number, array: number[]) {
   ;[array[i], array[j]] = [array[j], array[i]]
 }
 
-function insertionSort(array: number[]) {
+export default function insertionSort(array: number[]) {
   for (let i = 0; i < array.length - 1; i++) {
     for (let j = i + 1; j > 0; j--) {
       const current = array[j]
