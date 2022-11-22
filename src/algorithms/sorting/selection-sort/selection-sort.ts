6c8bcfb438cfa5b14eb7c6528567f4405f86ebe2
grzeznx2 <grzeznx2@gmail.com>
2022-11-22 20:57:49 +0100

    Add Tests for Selection Sort

--- src/algorithms/sorting/selection-sort/selection-sort.ts
+++ src/algorithms/sorting/selection-sort/selection-sort.ts
@@ -2,7 +2,7 @@ function swap(i: number, j: number, array: number[]) {
   ;[array[i], array[j]] = [array[j], array[i]]
 }
 
-function selectionSort(array: number[]) {
+export default function selectionSort(array: number[]) {
   for (let i = 0; i < array.length - 1; i++) {
     let currentMinIndex = i
     for (let j = i + 1; j < array.length; j++) {
